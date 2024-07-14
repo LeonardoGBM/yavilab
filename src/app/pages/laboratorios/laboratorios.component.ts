83d1a12efbf8658b41686cf57ea9944bd1c0b8d2
LeonardoGBM <leonardog.mh@outlook.com>
2024-07-14 00:58:28 -0500

    cambios

---
+++ src/app/pages/laboratorios/laboratorios.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-laboratorios',
+  standalone: true,
+  imports: [],
+  templateUrl: './laboratorios.component.html',
+  styleUrl: './laboratorios.component.css'
+})
+export class LaboratoriosComponent {
+
+}
