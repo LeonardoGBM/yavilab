f3f62842bdc7c2a0bc539ef6bab426e161aa344b
LeonardoGBM <leonardog.mh@outlook.com>
2024-07-14 01:18:42 -0500

    cambios

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,9 +3,11 @@ import { LoginComponent } from './login/login.component';
 
 import { SidebarComponent } from './layout/sidebar/sidebar.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { LaboratoriosComponent } from './pages/laboratorios/laboratorios.component';
 
 export const routes: Routes = [
     { path: '', component: LoginComponent},
     { path:'dashboard', component: DashboardComponent},
-    { path: 'sidebar', component: SidebarComponent}
+    { path: 'sidebar', component: SidebarComponent},
+    { path: '/laboratorios', component: LaboratoriosComponent}
 ];
