976a5fc0dab996407d43bdf461f5977174ee7a80
LeonardoGBM <leonardog.mh@outlook.com>
2024-07-14 00:10:04 -0500

    cambios

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,9 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
+import { SidebarComponent } from './layout/sidebar/sidebar.component';
 
 export const routes: Routes = [
-    { path: '', component: LoginComponent}
+    { path: '', component: LoginComponent},
+    { path: 'sidebar', component: SidebarComponent},
+
 ];
