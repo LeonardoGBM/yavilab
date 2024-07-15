00ae72542aab50364c6c84244a8b6b332c541fc6
LeonardoGBM <leonardog.mh@outlook.com>
2024-07-15 01:23:14 -0500

    cambios

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,13 @@ import { LoginComponent } from './login/login.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { LaboratoriosComponent } from './pages/laboratorios/laboratorios.component';
 import { EquiposComponent } from './pages/equipos/equipos.component';
+import { ErroresComponent } from './pages/errores/errores.component';
 
 export const routes: Routes = [
     { path: '', component: LoginComponent},
     { path: 'dashboard', component: DashboardComponent},
     { path: 'lab', component: LaboratoriosComponent},
-    { path: 'equipo', component: EquiposComponent}
+    { path: 'equipo', component: EquiposComponent},
+    { path: 'error', component: ErroresComponent}
 ];
 
--- src/app/pages/errores/errores.component.ts
+++ src/app/pages/errores/errores.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { SidebarComponent } from '../../layout/sidebar/sidebar.component';
 
 @Component({
   selector: 'app-errores',
   standalone: true,
-  imports: [],
+  imports: [SidebarComponent],
   templateUrl: './errores.component.html',
   styleUrl: './errores.component.css'
 })
