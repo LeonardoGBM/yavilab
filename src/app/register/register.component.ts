16a12cc2e169052ef0e70ed4d174ffbafe8647bc
Marcelo234 <98621883+Marcelo234@users.noreply.github.com>
2024-07-15 22:07:52 -0500

    todays

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -10,9 +10,26 @@ import { Router } from '@angular/router';
 })
 export class RegisterComponent {
 
-  constructor(private router: Router) {}
+  constructor(private router: Router) { }
 
   redirectToLogin() {
     this.router.navigate(['']);
+  }
+  showPassword: boolean = false;
+
+  togglePasswordVisibility(): void {
+    this.showPassword = !this.showPassword;
+    const passwordInput = document.getElementById('contrasena') as HTMLInputElement;
+    const toggleEye = document.getElementById('toggleEye') as HTMLElement;
+
+    if (this.showPassword) {
+      passwordInput.type = 'text';
+      toggleEye.classList.remove('bi-eye');
+      toggleEye.classList.add('bi-eye-slash');
+    } else {
+      passwordInput.type = 'password';
+      toggleEye.classList.remove('bi-eye-slash');
+      toggleEye.classList.add('bi-eye');
+    }
   }
 }
