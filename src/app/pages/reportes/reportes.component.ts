a6b52a1ec41425cff4d0970a9ff9c4a0cd1c9358
jgcARAUJO <ventasjosegc@hotmail.com>
2024-07-23 12:28:08 -0500

    -creacion de vista de daños, yavilab

--- src/app/pages/reportes/reportes.component.ts
+++ src/app/pages/reportes/reportes.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { SidebarComponent } from "../../layout/sidebar/sidebar.component";
 
 @Component({
   selector: 'app-reportes',
   standalone: true,
-  imports: [],
+  imports: [SidebarComponent],
   templateUrl: './reportes.component.html',
   styleUrl: './reportes.component.css'
 })
