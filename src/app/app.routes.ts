b77a38e8565d031a6b5c2c8a7319edf3bfbd7e35
jgcARAUJO <ventasjosegc@hotmail.com>
2024-07-23 21:55:12 -0500

    cambios corregidos

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,6 +5,11 @@ import { LaboratoriosComponent } from './pages/laboratorios/laboratorios.compone
 import { EquiposComponent } from './pages/equipos/equipos.component';
 import { RegisterComponent } from './register/register.component';
 import { ErroresComponent } from './pages/errores/errores.component';
+import { LabXianComponent } from './pages/lab-xian/lab-xian.component';
+import { LabGoriComponent } from './pages/lab-gori/lab-gori.component';
+import { LabToolouseComponent } from './pages/lab-toolouse/lab-toolouse.component';
+import { LabSarasotaComponent } from './pages/lab-sarasota/lab-sarasota.component';
+import { LabCenepaComponent } from './pages/lab-cenepa/lab-cenepa.component';
 
 export const routes: Routes = [
     { path: '', component: LoginComponent},
@@ -12,6 +17,11 @@ export const routes: Routes = [
     { path: 'lab', component: LaboratoriosComponent},
     { path: 'equipo', component: EquiposComponent},
     { path: 'register', component: RegisterComponent},
-    { path: 'errores', component: ErroresComponent}
+    { path: 'errores', component: ErroresComponent},
+    { path: 'lab-xian', component: LabXianComponent},
+    { path: 'lab-gori', component: LabGoriComponent},
+    { path: 'lab-toolouse', component:LabToolouseComponent},
+    { path: 'lab-sarasota', component:LabSarasotaComponent},
+    { path: 'lab-cenepa', component:LabCenepaComponent}
 ];
 
